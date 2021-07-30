55f323d211e7c31241227be469a7b4f6a117635e
joaobijos96 <joaopedrobijos96@gmail.com>
2021-07-30 13:32:49 -0300

    tests

--- Biblioteca/src/pages/home/index.js
+++ Biblioteca/src/pages/home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 
 import Header from '../../components/header/index';
+import CapaLivro  from '../../assets/coraline capa.png';
 
 export default () => {
     return(
@@ -9,6 +10,81 @@ export default () => {
             <Header/>
             <div className='content'>
                 <h1>Livros Disponíveis</h1>
+                <section className='rowList'>
+                    <h2>Bibliografia</h2>
+                    <div>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                    </div>
+                </section>
+                <section className='rowList'>
+                    <h2>Romance</h2>
+                    <div>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                    </div>
+                </section>
+                <section className='rowList'>
+                    <h2>Romance</h2>
+                    <div>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                        <img src={CapaLivro}/>
+                    </div>
+                </section>
             </div>
         </div>
     )
